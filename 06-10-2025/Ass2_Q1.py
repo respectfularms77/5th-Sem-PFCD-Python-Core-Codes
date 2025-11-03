576ecbcbf1d02abcc4ffd70b684ad7fd58d7ed93
Arpit Kumar Mohanty <147530265+respectfularms77@users.noreply.github.com>
2025-11-03 11:21:46 +0530

    Delete 06-10-2025 directory

--- 06-10-2025/Ass2_Q1.py
+++
@@ -1,5 +0,0 @@
-st = input("Is it Raining there ?: ")
-if(st == "yes"):
-    print("Carry an Umbrella")
-else:
-    print("Bye")
--- 06-10-2025/Ass2_Q2.py
+++
@@ -1,7 +0,0 @@
-st = input("Is it Raining there ?: ")
-if(st == "yes"):
-    print("Carry an Umbrella")
-elif (st == "no"):
-    print("No need to carry an Umbrella")
-else:
-    print("Bye")
